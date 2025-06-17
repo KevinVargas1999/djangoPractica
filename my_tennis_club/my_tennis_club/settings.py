6f44468fcefcefebbc9627bd3e11cfb71d5f9003
Kevin Vargas <kevvbena@espol.edu.ec>
2025-06-17 08:16:47 -0500

    me quede hasta agregar vista de prueba

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7!(ycq()bfck0y8(^l6z&fz$a$mjl8t8u@@n(s$206!q2!lf_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
